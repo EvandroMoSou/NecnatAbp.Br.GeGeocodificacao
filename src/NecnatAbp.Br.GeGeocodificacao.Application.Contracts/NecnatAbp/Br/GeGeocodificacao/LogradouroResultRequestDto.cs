761318c2cbef635f581effeb68f3a8d6ee41dc73
Evandro Mota de Souza <emotasouza@PRODAM.SP.GOV.BR>
2024-01-30 14:44:13 -0300

    Logradouro, Geolocalizacao

---
+++ src/NecnatAbp.Br.GeGeocodificacao.Application.Contracts/NecnatAbp/Br/GeGeocodificacao/LogradouroResultRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using NecnatAbp.Dtos;
+using System;
+using Volo.Abp.Application.Dtos;
+
+namespace NecnatAbp.Br.GeGeocodificacao
+{
+    public class LogradouroResultRequestDto : OptionalPagedAndSortedResultRequestDto
+    {
+        public string? GenericSearch { get; set; }
+        public string? Cep { get; set; }
+        public string? NomeContains { get; set; }
+        public Guid? BairroDistritoId { get; set; }
+        public Guid? CidadeMunicipioId { get; set; }
+        public UnidadeFederativa? UnidadeFederativa { get; set; }
+        public bool? Ativo { get; set; }
+    }
+}
